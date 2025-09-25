fbbedcde225658069c70c4b23d003f2a1ff7c996
Pontus <pontus.kungsbacka@gmail.com>
2025-09-25 14:43:30 +0200

    reading in test point, cleaning and plotting the test point with the data

--- Lab2.py
+++ Lab2.py
@@ -56,3 +56,48 @@ testpoints = np.array(testpoints)
 
 def euclidean_distance(p1, p2):
     return np.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
+
+# --- Classify - test points with 1-nn ---
+classified_points = []
+for i, test in enumerate(testpoints):
+    distancesTestpoints = [] # Creating a list for distance
+    for train in data:
+        dist = euclidean_distance(test, train[:2])
+        distancesTestpoints.append((dist, int(train[2])))
+    #print(distances) check whats in the list
+    # Sort distance with closest label 0 or 1
+    distancesTestpoints.sort(key=lambda x: x[0])
+    nearest_label = distancesTestpoints[0][1]
+    predicted = "Pichu" if nearest_label == 0 else "Pikachu"
+    classified_points.append((test[0], test[1], predicted))
+
+
+    # Write out distances with closest label 
+    print(f"Test points {i+1} with (width, height): {test[0]}, {test[1]} predicted as: {predicted}")
+    
+# --- Make a new graf for checking the test data ---
+plt.scatter(pichu[:, 0], pichu[:, 1], color="#FFF06A", label="Pichu (train)")
+plt.scatter(pikachu[:, 0], pikachu[:, 1], color="#E19720", label="Pikachu (train)")
+
+# --- Putting in test point nearest neaber data in plot with stars ---
+shown = set()  # Keeps track of which classes are already in the scatter plot
+
+# --- For Loop to add test point with closest data point label --- 
+for (width, height, lab) in classified_points:
+    color = "#FFF06A" if lab == "Pichu" else "#E19720"
+    # Giving correct color depending on value
+    # Using "_nolegend_" so we don't get duplicates. And shortened label to lbl
+    lbl = f"Test point ({lab})" if lab not in shown else "_nolegend_"
+
+    plt.scatter(width, height,
+                color=color, marker="*", s=250, edgecolors="black",
+                label=lbl)
+
+    # Add test data point to scatter plot
+    shown.add(lab)
+
+plt.xlabel("Width (cm)")
+plt.ylabel("Height (cm)")
+plt.title("Classification of test points (1-NN)")
+plt.legend()
+plt.show()
